7eb33f53efb8d3f804fae251eda0686256b8a3aa
Tinyang <1251307117@qq.com>
2018-05-25 12:10:55 +0800

    yang project first commit

---
+++ src/main/java/com/noosh/yang/YangApplication.java
@@ -0,0 +1,12 @@
+package com.noosh.yang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YangApplication.class, args);
+	}
+}

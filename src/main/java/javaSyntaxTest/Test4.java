b05fb55069cb451ffd13b24cc3e67e9c79b6a9dc
Tinyang <1251307117@qq.com>
2019-01-08 14:06:01 +0800

    test2

--- src/main/java/javaSyntaxTest/Test4.java
+++ src/main/java/javaSyntaxTest/Test4.java
@@ -3,7 +3,7 @@ package javaSyntaxTest;
 public class Test4 {
 
     public static final int SPRING = 0;
-    public static final int SUMMER = 1;
+    public static final int SUMMER = 111;
     public static final int AUTUMN = 2;
     public static final int WINTER = 4;
 

dcd824f5cc722c91aa2a8e7b6e04a446585af971
Tinyang <1251307117@qq.com>
2019-01-08 14:03:14 +0800

    test1

--- src/main/java/javaSyntaxTest/Test4.java
+++ src/main/java/javaSyntaxTest/Test4.java
@@ -5,7 +5,7 @@ public class Test4 {
     public static final int SPRING = 0;
     public static final int SUMMER = 1;
     public static final int AUTUMN = 2;
-    public static final int WINTER = 4;
+    public static final int WINTER = 4444;
 
     public static void main(String[] args) {
         System.out.println(Test3.SPRING1);

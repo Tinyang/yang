b38db49b638951de6d3fa968efee4740642e9129
Tinyang <1251307117@qq.com>
2020-02-05 09:22:19 +0800

    spring security

--- src/main/java/study/syntax/Test8.java
+++ src/main/java/study/syntax/Test8.java
@@ -3,16 +3,21 @@ package study.syntax;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Test8 {
 
     public static void main(String[] args) {
-        Velocity.init();
-        VelocityContext velocityContext = new VelocityContext();
-        velocityContext.put("test","testValue");
-        Template template = null;
-        Velocity.getTemplate("testTemplate.vm");
+        HashSet set = new HashSet();
 
+        set.add(new Integer(1));
+        set.add(new Integer(2));
+        set.add(new Integer(3));
+        set.add(new Integer(4));
 
     }
 }
